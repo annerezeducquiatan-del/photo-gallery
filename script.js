4d295970b787950cf81e0061b0efccfe30f26c6e
annerezeducquiatan-del <annerezeducquiatan@gmail.com>
2026-01-15 00:00:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-function upDate(previewPic) {
-    console.log("Hovered on:", previewPic);
 
+function upDate(previewPic) {
+  
     let mainDiv = document.getElementById("image");
 
     mainDiv.innerHTML = previewPic.alt;
 
+   
     mainDiv.style.backgroundImage = `url('${previewPic.src}')`;
 }
+
 
 function undo() {
     let mainDiv = document.getElementById("image");
 
+
     mainDiv.innerHTML = "Hover over an image below to display here.";
+
 
     mainDiv.style.backgroundImage = "url('')";
 }
