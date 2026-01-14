6a46c949ff0d4f1ec66f0622ecfe5bbeefc3689f
annerezeducquiatan-del <annerezeducquiatan@gmail.com>
2026-01-15 00:11:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-
+// Function to update main display when hovering on a thumbnail
 function upDate(previewPic) {
-  
     let mainDiv = document.getElementById("image");
-
+    
+    // Update text to the alt of the image
     mainDiv.innerHTML = previewPic.alt;
-
-   
+    
+    // Update background image
     mainDiv.style.backgroundImage = `url('${previewPic.src}')`;
+    mainDiv.style.backgroundSize = "cover";   // Cover entire div
+    mainDiv.style.backgroundPosition = "center";
 }
 
-
+// Function to reset main display when mouse leaves thumbnail
 function undo() {
     let mainDiv = document.getElementById("image");
-
-
     mainDiv.innerHTML = "Hover over an image below to display here.";
-
-
-    mainDiv.style.backgroundImage = "url('')";
+    mainDiv.style.backgroundImage = "";
 }
